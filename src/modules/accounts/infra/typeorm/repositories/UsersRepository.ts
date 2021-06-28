8a8ce104d06d82714572638c1af8760b316c6225
Luiz Fernando Martins Leite <luizfernandoleite@gmail.com>
2021-06-28 12:19:02 -0300

    cors

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -6,41 +6,41 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepositor
 import { User } from "../entities/User";
 
 class UsersRepository implements IUsersRepository {
-    private respository: Repository<User>;
+  private respository: Repository<User>;
 
-    constructor() {
-        this.respository = getRepository(User);
-    }
+  constructor() {
+    this.respository = getRepository(User);
+  }
 
-    async create({
-        name,
-        email,
-        driver_license,
-        password,
-        avatar,
-        id,
-    }: ICreateUserDTO): Promise<void> {
-        const user = this.respository.create({
-            name,
-            email,
-            driver_license,
-            password,
-            avatar,
-            id,
-        });
+  async create({
+    name,
+    email,
+    driver_license,
+    password,
+    avatar,
+    id,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.respository.create({
+      name,
+      email,
+      driver_license,
+      password,
+      avatar,
+      id,
+    });
 
-        await this.respository.save(user);
-    }
-    async findByEmail(email: string): Promise<User> {
-        const user = await this.respository.findOne({ email });
+    await this.respository.save(user);
+  }
+  async findByEmail(email: string): Promise<User> {
+    const user = await this.respository.findOne({ email });
 
-        return user;
-    }
-    async findById(id: string): Promise<User> {
-        const user = await this.respository.findOne(id);
+    return user;
+  }
+  async findById(id: string): Promise<User> {
+    const user = await this.respository.findOne(id);
 
-        return user;
-    }
+    return user;
+  }
 }
 
 export { UsersRepository };
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
@@ -23,6 +24,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/car", express.static(`${upload.tmpFolder}/car`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
